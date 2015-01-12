20ef48df75378687ac43b194829c27de0914cda3
Tony Montana <js.pwns@gmail.com>
2015-01-12 06:58:22 -0500

    Replace tabs with spaces and implement CaesarChiper

--- Extender/Security/Cryptography/CRC32CryptoServiceProvider.cs
+++ Extender/Security/Cryptography/CRC32CryptoServiceProvider.cs
@@ -1,91 +1,91 @@
 ﻿namespace System.Security.Cryptography
 {
-	public class CRC32CryptoServiceProvider : HashAlgorithm
-	{
-		public const uint DEFAULT_POLYNOMIAL = 0xEDB88320;
-		public const uint DEFAULT_SEED       = 0xFFFFFFFF;
+    public class CRC32CryptoServiceProvider : HashAlgorithm
+    {
+        public const uint DEFAULT_POLYNOMIAL = 0xEDB88320;
+        public const uint DEFAULT_SEED       = 0xFFFFFFFF;
 
-		private uint mPolynomial;
-		private uint mSeed;
-		private uint mHash;
-		private uint[] mTable;
+        private uint mPolynomial;
+        private uint mSeed;
+        private uint mHash;
+        private uint[] mTable;
 
-		public override int HashSize
-		{
-			get { return 32; }
-		}
+        public override int HashSize
+        {
+            get { return 32; }
+        }
 
-		public CRC32CryptoServiceProvider() : this( DEFAULT_POLYNOMIAL, DEFAULT_SEED ) { }
-		public CRC32CryptoServiceProvider( uint iPolynomial ) : this( DEFAULT_POLYNOMIAL, DEFAULT_SEED ) { }
-		public CRC32CryptoServiceProvider( uint iPolynomial, uint iSeed )
-		{
-			this.mPolynomial = iPolynomial;
-			this.mSeed = iSeed;
-			this.mHash = iSeed;
+        public CRC32CryptoServiceProvider() : this( DEFAULT_POLYNOMIAL, DEFAULT_SEED ) { }
+        public CRC32CryptoServiceProvider( uint iPolynomial ) : this( DEFAULT_POLYNOMIAL, DEFAULT_SEED ) { }
+        public CRC32CryptoServiceProvider( uint iPolynomial, uint iSeed )
+        {
+            this.mPolynomial = iPolynomial;
+            this.mSeed = iSeed;
+            this.mHash = iSeed;
             this.Initialize();
-		}
+        }
 
         public override void Initialize()
         {
             this.mTable = InitializeTable( DEFAULT_POLYNOMIAL );
         }
 
-		private static uint[] InitializeTable( uint iPolynomial )
-		{
-			uint[] iTable = new uint[256];
+        private static uint[] InitializeTable( uint iPolynomial )
+        {
+            uint[] iTable = new uint[256];
 
-			for( int i = 0; i < 256; ++i )
-			{
-				uint k = (uint)i;
+            for( int i = 0; i < 256; ++i )
+            {
+                uint k = (uint)i;
 
-				for( int j = 0; j < 8; ++i )
-				{
-					if( ( k & 1 ) == 1 )
-						k = ( k >> 1 ) ^ iPolynomial;
-					else
-						k = k >> 1;
-				}
+                for( int j = 0; j < 8; ++i )
+                {
+                    if( ( k & 1 ) == 1 )
+                        k = ( k >> 1 ) ^ iPolynomial;
+                    else
+                        k = k >> 1;
+                }
 
-				iTable[i] = k;
-			}
+                iTable[i] = k;
+            }
 
-			return iTable;
-		}
+            return iTable;
+        }
 
-		private static uint CalculateHash( byte[] array, int ibStart, int cbSize, uint seed, uint[] table, bool flip )
-		{
-			uint CRC = seed;
+        private static uint CalculateHash( byte[] array, int ibStart, int cbSize, uint seed, uint[] table, bool flip )
+        {
+            uint CRC = seed;
 
-			for( int i = ibStart; i < cbSize; ++i )
-				unchecked { CRC = ( CRC >> 8 ) ^ table[array[i] ^ CRC & 0xFF]; }
+            for( int i = ibStart; i < cbSize; ++i )
+                unchecked { CRC = ( CRC >> 8 ) ^ table[array[i] ^ CRC & 0xFF]; }
 
-			if( flip )
-				return ~CRC;
-			else
-				return CRC;
-		}
+            if( flip )
+                return ~CRC;
+            else
+                return CRC;
+        }
 
-		private byte[] GetBigEndianBytes( uint value )
-		{
-			return new byte[]
-			{
-				(byte)( ( value >> 24 ) & 0xFF ),
-				(byte)( ( value >> 16 ) & 0xFF ),
-				(byte)( ( value >> 8 ) & 0xFF ),
-				(byte)( value & 0xFF )
-			};
-		}
+        private byte[] GetBigEndianBytes( uint value )
+        {
+            return new byte[]
+            {
+                (byte)( ( value >> 24 ) & 0xFF ),
+                (byte)( ( value >> 16 ) & 0xFF ),
+                (byte)( ( value >> 8 ) & 0xFF ),
+                (byte)( value & 0xFF )
+            };
+        }
 
-		protected override void HashCore( byte[] array, int ibStart, int cbSize )
-		{
-			this.mHash = CalculateHash( array, ibStart, cbSize, DEFAULT_SEED, InitializeTable( DEFAULT_POLYNOMIAL ), false );	
-		}
+        protected override void HashCore( byte[] array, int ibStart, int cbSize )
+        {
+            this.mHash = CalculateHash( array, ibStart, cbSize, DEFAULT_SEED, InitializeTable( DEFAULT_POLYNOMIAL ), false );    
+        }
 
-		protected override byte[] HashFinal()
-		{
-			byte[] array = GetBigEndianBytes( ~this.mHash );
-			this.HashValue = array;
-			return array;
-		}
+        protected override byte[] HashFinal()
+        {
+            byte[] array = GetBigEndianBytes( ~this.mHash );
+            this.HashValue = array;
+            return array;
+        }
     }
 }
--- Extender/Security/Cryptography/CaesarCipher.cs
+++ Extender/Security/Cryptography/CaesarCipher.cs
@@ -1,6 +1,30 @@
 ﻿namespace System.Security.Cryptography
 {
-	public static class CaesarCipher
-	{
-	}
+    public static class CaesarCipher
+    {
+        public static string Encode( string value, int shift )
+        {
+            var chars = value.ToCharArray();
+            var len = chars.Length;
+
+            for( var i = 0; i < len; ++i )
+            {
+                var c = (char)( chars[i] + shift );
+
+                if( c > 'z' )
+                    c = (char)( c - 26 );
+                else if( c < 'a' )
+                    c = (char)( c + 26 );
+
+                chars[i] = c;
+            }
+
+            return new String( chars );
+        }
+
+        public static string Decode( string value, int shift )
+        {
+            return CaesarCipher.Encode( value, shift < 0 ? shift : -shift );
+        }
+    }
 }
